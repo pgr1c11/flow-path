0701fcbe18a0b35fb187e00a4e4b3de257a5c6d6
PR-Pers <pgredshaw@hotmail.co.uk>
2022-07-14 23:33:55 +0100

    feat: add map from bbox method

--- src/map.py
+++ src/map.py
@@ -1,19 +1,30 @@
+from typing import List, Tuple
+import collections
+import functools
+import operator
 from src.results import MapResults
 from src.controls import Controls
 from src.cell import Cell
-from src.path import Path
 from src.flow import Flow
-from typing import List, Tuple
 
 
 class Map:
     def __init__(self, controls: Controls) -> None:
         self.__controls = controls
         self.results: MapResults
-        self.temp: List[Flow]
 
     def from_whole_dem(self) -> None:
         pass
+
+    def __combine_flows(self, flows: List[Flow]) -> dict:
+        flow_dict = [
+            {
+                f.results.affected_cells[i]: f.results.cell_values[i]
+                for i in range(len(f.results.affected_cells))
+            }
+            for f in flows
+        ]
+        return dict(functools.reduce(operator.add, map(collections.Counter, flow_dict)))
 
     def from_box(
         self, top_left_coords: Tuple[int, int], bottom_right_coords: Tuple[int, int]
@@ -24,7 +35,14 @@ class Map:
             Cell(controls=self.__controls, coords=(r, c)) for r in rows for c in cols
         ]
         flows = [Flow(controls=self.__controls, start_cell=cell) for cell in cells]
-        self.temp = flows
+        collapsed_flows = self.__combine_flows(flows=flows)
+        affected_cells = list(collapsed_flows.keys())
+        cell_values = list(collapsed_flows.values())
+        self.results = MapResults(
+            controls=self.__controls,
+            affected_cells=affected_cells,
+            cell_values=cell_values,
+        )
 
     def from_raster(self) -> None:
         pass
--- src/results.py
+++ src/results.py
@@ -6,7 +6,8 @@ import os
 from matplotlib import pyplot as plt
 from src.controls import Controls
 
-ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+
 
 class Results(ABC):
     def __init__(self) -> None:
@@ -14,7 +15,7 @@ class Results(ABC):
         self.affected_cells: list
         self.cell_values: list
         self.array_shape: list
-    
+
     def _get_array_shape(self) -> list:
         r_vals = [i[0] for i in self.affected_cells]
         c_vals = [i[1] for i in self.affected_cells]
@@ -36,28 +37,42 @@ class Results(ABC):
             return 27700
         else:
             proj = osr.SpatialReference(self.controls.dem.crs)
-            return int(proj.GetAttrValue('AUTHORITY',1))
+            return int(proj.GetAttrValue("AUTHORITY", 1))
 
     def create_array(self) -> np.ndarray:
-        r_min, c_min, rows, cols = self.array_shape[0], self.array_shape[2], self.array_shape[4], self.array_shape[5]
+        r_min, c_min, rows, cols = (
+            self.array_shape[0],
+            self.array_shape[2],
+            self.array_shape[4],
+            self.array_shape[5],
+        )
         arr = np.zeros(shape=(rows, cols))
-        affected_cells_floor = [(i[0] - r_min, i[1] - c_min) for i in self.affected_cells]
+        affected_cells_floor = [
+            (i[0] - r_min, i[1] - c_min) for i in self.affected_cells
+        ]
         affected_cells_array = np.array(affected_cells_floor)
-        arr[(affected_cells_array[:,0], affected_cells_array[:,1])] = self.cell_values
+        arr[(affected_cells_array[:, 0], affected_cells_array[:, 1])] = self.cell_values
         return arr
 
-    def create_raster(self, filename = 'temp.tif') -> None:
-        r_min, c_min, rows, cols = self.array_shape[0], self.array_shape[2], self.array_shape[4], self.array_shape[5]
+    def create_raster(self, filename="temp.tif") -> None:
+        r_min, c_min, rows, cols = (
+            self.array_shape[0],
+            self.array_shape[2],
+            self.array_shape[4],
+            self.array_shape[5],
+        )
         arr = self.create_array()
         origin = self.__get_origin(r_min=r_min, c_min=c_min)
         origin_x, origin_y = origin[0], origin[1]
         pixel_size = self.controls.dem.resolution
         pixel_size_x, pixel_size_y = pixel_size[0], pixel_size[1]
-        temp_filename = os.path.join(ROOT_DIR, 'data', 'wip', filename)
+        temp_filename = os.path.join(ROOT_DIR, "data", "wip", filename)
         epsg_code = self.__get_epsg_code()
-        driver = gdal.GetDriverByName('GTiff')
+        driver = gdal.GetDriverByName("GTiff")
         outRaster = driver.Create(temp_filename, cols, rows, 1, gdal.GDT_Float32)
-        outRaster.SetGeoTransform((origin_x, pixel_size_x, 0, origin_y, 0, pixel_size_y))
+        outRaster.SetGeoTransform(
+            (origin_x, pixel_size_x, 0, origin_y, 0, pixel_size_y)
+        )
         outband = outRaster.GetRasterBand(1)
         outband.WriteArray(arr)
         outRasterSRS = osr.SpatialReference()
@@ -66,9 +81,10 @@ class Results(ABC):
         outband.FlushCache()
 
     def plot(self) -> None:
-        plt.imshow(self.create_array(), cmap='pink')
+        plt.imshow(self.create_array(), cmap="pink")
         plt.colorbar()
         plt.show()
+
 
 class PathResults(Results):
     def __init__(self, controls: Controls, affected_cells: list) -> None:
@@ -77,12 +93,22 @@ class PathResults(Results):
         self.cell_values = [1 for _ in self.affected_cells]
         self.array_shape = self._get_array_shape()
 
+
 class FlowResults(Results):
-    def __init__(self, controls: Controls, affected_cells: list, cell_values: list) -> None:
+    def __init__(
+        self, controls: Controls, affected_cells: list, cell_values: list
+    ) -> None:
         self.controls = controls
         self.affected_cells = affected_cells
         self.cell_values = cell_values
         self.array_shape = self._get_array_shape()
 
-class MapResults:
-    pass
+
+class MapResults(Results):
+    def __init__(
+        self, controls: Controls, affected_cells: list, cell_values: list
+    ) -> None:
+        self.controls = controls
+        self.affected_cells = affected_cells
+        self.cell_values = cell_values
+        self.array_shape = self._get_array_shape()
